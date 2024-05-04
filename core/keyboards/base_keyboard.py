42fb4097ba06d4476d747981e2701ddb145ad9db
alllexxx1 <alekkorpachyov@gmail.com>
2024-05-04 16:39:06 +0300

    Add the keyboard with two options. Add a command section with one basic command "/start"

---
+++ core/keyboards/base_keyboard.py
@@ -0,0 +1,16 @@
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+def get_base_keyboard():
+    keyboard_builder = ReplyKeyboardBuilder()
+
+    keyboard_builder.button(text='Good first issues')
+    keyboard_builder.button(text='Dice')
+
+    keyboard_builder.adjust(2)
+
+    return keyboard_builder.as_markup(
+        resize_keyboard=True,
+        one_time_keyboard=False,
+        input_field_placeholder='Opt for an option'
+    )
--- core/main.py
+++ core/main.py
@@ -4,10 +4,17 @@ import logging
 import sys
 
 from aiogram import Dispatcher, Bot, F
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 from handlers.basic import send_dice
 from handlers.git_issues import show_issues
+
+from keyboards.base_keyboard import get_base_keyboard
+
 from project_config import config
+
+from utils.commands import set_commands
 
 
 BOT_TOKEN = config.bot_token.get_secret_value()
@@ -18,7 +25,8 @@ dp = Dispatcher()
 
 
 @dp.startup()
-async def start_bot():
+async def start_bot(bot: Bot):
+    await set_commands(bot)
     await bot.send_message(chat_id=ADMIN_ID, text="I'm alive")
 
 
@@ -27,12 +35,21 @@ async def stop_bot():
     await bot.send_message(chat_id=ADMIN_ID, text="I'm done")
 
 
+async def cmd_start(message: Message):
+    await message.answer(f'Hello, {message.from_user.full_name} !',
+                         reply_markup=get_base_keyboard())
+
+
 async def main():
+    dp.message.register(cmd_start, CommandStart())
     dp.message.register(
         show_issues,
         F.text == 'Good first issues'
     )
-    dp.message.register(send_dice)
+    dp.message.register(
+        send_dice,
+        F.text == 'Dice'
+    )
 
     try:
         await dp.start_polling(bot)
